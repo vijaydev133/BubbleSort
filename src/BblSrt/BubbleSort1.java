e3b099c25286b4dc58fcb8f7d884b732eb82bd1d
vijaydev133 <vijaydeveloper004@gmail.com>
2023-01-13 16:36:43 +0530

    new push

--- src/BblSrt/BubbleSort1.java
+++ src/BblSrt/BubbleSort1.java
@@ -12,12 +12,17 @@ public class BubbleSort1 {
     }
 
     private static void bubblesort(int[] arr) {
+        boolean isSwapped = false;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length - i; j++) {
                 if(arr[j] < arr[j-1]){
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
+                    isSwapped = true;
+                }
+                if(!isSwapped){
+                    break;
                 }
             }
         }
