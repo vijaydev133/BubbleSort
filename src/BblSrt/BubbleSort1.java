3fe62f937d78785f6a79b444f87a6d2731ddcef7
vijaydev133 <vijaydeveloper004@gmail.com>
2023-01-13 16:32:48 +0530

    new push

--- src/BblSrt/BubbleSort1.java
+++ src/BblSrt/BubbleSort1.java
@@ -1,4 +1,25 @@
 package BblSrt;
+//stable unstable
+
+import java.util.Arrays;
 
 public class BubbleSort1 {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+
+        bubblesort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubblesort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
 }
